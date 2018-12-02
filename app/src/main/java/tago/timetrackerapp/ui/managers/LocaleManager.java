d36c9de1399252eed0e635dc3cfaffad1b404a9b
Tiago Redaelli <tiagor@kth.se>
2018-12-02 23:21:50 +0100

    - documentation LocaleManager

--- app/src/main/java/tago/timetrackerapp/ui/managers/LocaleManager.java
+++ app/src/main/java/tago/timetrackerapp/ui/managers/LocaleManager.java
@@ -13,16 +13,31 @@ public class LocaleManager {
 
     private static String cachedLanguage;
 
+    /**
+     * Resets the current Locale language to the one stored in cache, used to override the default
+     * user language. This means that it must be called every time the layout is drawn.
+     * @param c context
+     */
     public static void setLocale(Context c) {
         setNewLocale(c, getLanguage(c));
     }
 
+    /**
+     * Changes the language of the current Locale. Use this to change the current language of the
+     * Locale.
+     * @param c context
+     * @param language 2 letter Locale country / language
+     */
     public static void setNewLocale(Context c, String language) {
         cachedLanguage = language;
         updateResources(c, language);
     }
 
-
+    /**
+     * Returns the current cached language.
+     * @param c context
+     * @return 2 letter Locale country / language
+     */
     public static String getLanguage(Context c) {
         return cachedLanguage;
     }
